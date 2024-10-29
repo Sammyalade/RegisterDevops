3f35d564a9f6d82d19906f21c73d3b326b2b9b7b
Aladeyelu Olufemi Samuel <151362938+Sammyalade@users.noreply.github.com>
2024-10-29 12:56:56 +0100

    fibonacci for scaling

--- src/main/java/com/register/Register/model/User.java
+++ src/main/java/com/register/Register/model/User.java
@@ -1,15 +1,13 @@
 package com.register.Register.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Data
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = IDENTITY)
