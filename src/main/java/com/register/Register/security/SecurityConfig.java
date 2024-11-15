35f44dd0750974d667e92d6a6d53924bfc87c3ab
Aladeyelu Olufemi Samuel <151362938+Sammyalade@users.noreply.github.com>
2024-11-15 14:41:51 +0100

    build-and-deploy

---
+++ src/main/java/com/register/Register/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.register.Register.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .sessionManagement(c->c.sessionCreationPolicy(STATELESS))
+                .authorizeHttpRequests(c->
+                        c.requestMatchers(POST,"/api/v1/register").permitAll())
+                .build();
+    }
+}
