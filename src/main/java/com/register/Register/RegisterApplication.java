2ab4660dc707199443d608e69013a8ab38b6174b
Aladeyelu Olufemi Samuel <151362938+Sammyalade@users.noreply.github.com>
2024-11-15 11:15:51 +0100

    build-and-deploy

--- src/main/java/com/register/Register/RegisterApplication.java
+++ src/main/java/com/register/Register/RegisterApplication.java
@@ -1,6 +1,5 @@
 package com.register.Register;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
